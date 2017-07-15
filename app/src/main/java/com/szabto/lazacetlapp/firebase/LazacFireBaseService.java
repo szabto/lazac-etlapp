7a7610198efc936a882e1071161245b7d1c4fc88
szabto <szabto@gmail.com>
2017-07-16 00:26:15 +0200

    Added refresh token handling

--- app/src/main/java/com/szabto/lazacetlapp/firebase/LazacFireBaseService.java
+++ app/src/main/java/com/szabto/lazacetlapp/firebase/LazacFireBaseService.java
@@ -4,6 +4,14 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.szabto.lazacetlapp.api.responses.ResponseBase;
+import com.szabto.lazacetlapp.helpers.ApiHelper;
+import com.szabto.lazacetlapp.helpers.FavoriteHelper;
+import com.szabto.lazacetlapp.structures.Command;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by root on 3/24/17.
@@ -26,5 +34,16 @@ public class LazacFireBaseService extends FirebaseInstanceIdService {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
+        ApiHelper api = new ApiHelper(this);
+        api.getService().registerUUID(FavoriteHelper.getInstance().getUserToken(), refreshedToken).enqueue(new Callback<ResponseBase>() {
+            @Override
+            public void onResponse(Call<ResponseBase> call, Response<ResponseBase> response) {
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBase> call, Throwable t) {
+
+            }
+        });
     }
 }
